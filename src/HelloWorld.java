67c726206b2cbd5d12e0b6381b417d96459114d4
DivakarD-QA <ddiva555kar@gmail.com>
2024-04-20 11:02:58 +0530

    Homework

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,6 +5,19 @@ public class HelloWorld {
         System.out.print("Hello World\n");
         System.out.print("Hello World\t" + "TEst");
 
+        String m = "";
+        double x = Math.sqrt(144);
+        double y = Math.sqrt(25);
+        double z = Math.sqrt(100);
+        double a = x + y + z;
+        double result = Math.cbrt(a);
+        System.out.println("\n" + "x =" + x);
+        System.out.println("y =" + y);
+        System.out.println("z =" + z);
+        System.out.println( "a = " + a);
+        System.out.println("result =" + result);
+
+
 
 
 
